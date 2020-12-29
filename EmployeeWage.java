56b1ccdf91c0401532a04d7197dbca3e8a855476
AniketGujar <gujaraniket7875@gmail.com>
2020-12-29 23:54:09 +0530

    Added: Calculate wages till days/hrs condition is reached

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -7,12 +7,13 @@ public class EmployeeWage {
 	static int part_Time_Wage = 20;
 	static int working_Days = 20;
 	static int TotalWage;
+	static int totalHours;
 	
 		public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation Program");
 		Random rand = new Random();
 		
-		for(int day=0; day<working_Days; day++) {
+		for(int day=0; day<working_Days || totalHours<100; day++) {
 		
 		int random = rand.nextInt(3);
 		if(random==0)
@@ -24,29 +25,34 @@ public class EmployeeWage {
 				case 1:
 					System.out.println("Part Time Wage = " + part_Time_Wage);
 					TotalWage=TotalWage+ part_Time_Wage;
+					totalHours=totalHours+1;
 					break;
                 case 2:
                     System.out.println("Part Time Wage = " + part_Time_Wage*2);
                     TotalWage=TotalWage+ (part_Time_Wage*2);
+                    totalHours=totalHours+2;
                     break;
                 case 3:
                     System.out.println("Part Time Wage = " + part_Time_Wage*3);
                     TotalWage=TotalWage+ (part_Time_Wage*3);
+                    totalHours=totalHours+3;
                     break;
                 case 0:
                 	System.out.println("Part Time Wage = " + part_Time_Wage*4);
                 	TotalWage=TotalWage+ (part_Time_Wage*4);
+                	totalHours=totalHours+4;
 			}
 		}
 		else if (random==1){
 			System.out.println("Employee is Present");
 			System.out.println("Daily Wage = "+ full_Day_Wage);
 			TotalWage=TotalWage+ full_Day_Wage;
+			totalHours=totalHours+8;
 		}
 		else {
 			System.out.println("Employee Absent");
 		}
 		}
-		System.out.println("Monthly Wage of Employee = " + TotalWage);
+		System.out.println(totalHours+"Monthly Wage of Employee = " + TotalWage);
 		}
 }
