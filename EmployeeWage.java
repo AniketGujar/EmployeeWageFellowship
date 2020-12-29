2bf0a016cf8239062c1aca19bfdc847dbb1dd461
AniketGujar <gujaraniket7875@gmail.com>
2020-12-29 22:23:13 +0530

    Added: Full Day Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,6 +3,7 @@ package EmployeeWageBuilder;
 import java.util.Random;
 
 public class EmployeeWage {
+	static int full_Day_Wage = 160;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation Program");
@@ -11,10 +12,12 @@ public class EmployeeWage {
 		if(random==0)
 		{
 			System.out.println("Employee is Absent");
+			System.out.println("Daily Wage = "+ 0);
 		}
 		else
 		{
 			System.out.println("Employee is Present");
+			System.out.println("Daily Wage = "+ full_Day_Wage);
 		}
 	}
 }
