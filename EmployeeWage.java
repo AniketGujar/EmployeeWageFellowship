ecb627d54c4b8b2cd2dc831852b2f63005c0941e
AniketGujar <gujaraniket7875@gmail.com>
2020-12-29 23:27:20 +0530

    Added: Switch Case to check condition

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,21 +4,38 @@ import java.util.Random;
 
 public class EmployeeWage {
 	static int full_Day_Wage = 160;
-	static int part_Time_Wage = 80;
+	static int part_Time_Wage = 20;
 	
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		System.out.println("Welcome to Employeewage Computation Program");
 		Random rand = new Random();
-		int random = rand.nextInt(2);
+		int random = rand.nextInt(3);
 		if(random==0)
 		{
-			System.out.println("Employee is Absent");
-			System.out.println("Daily Wage = "+ 0);
+			System.out.println("Employee is Present Part Time");
+			
+		int partTimeCheck = rand.nextInt(4);	
+			switch (partTimeCheck) {
+				case 1:
+					System.out.println("Part Time Wage = " + part_Time_Wage);
+					break;
+                case 2:
+                    System.out.println("Part Time Wage = " + part_Time_Wage*2);
+                    break;
+                case 3:
+                    System.out.println("Part Time Wage = " + part_Time_Wage*3);
+                    break;
+                case 0:
+                	System.out.println("Part Time Wage = " + part_Time_Wage*4);
+			}
 		}
-		else
-		{
+		else if (random==1){
 			System.out.println("Employee is Present");
 			System.out.println("Daily Wage = "+ full_Day_Wage);
 		}
+		else {
+			System.out.println("Employee Absent");
+		}
+		
 	}
 }
